cb7c7427f64a8342ce12ca9efd98d04cc90b215a
Andrets <dmitriysurkov2019@gmail.com>
2024-03-16 21:11:20 +0500

    init microservice and protobuffs

---
+++ src/auth/auth.pb.ts
@@ -0,0 +1,102 @@
+/* eslint-disable */
+import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
+import { Observable } from "rxjs";
+
+export const protobufPackage = "auth";
+
+export interface RegisterRequest {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface RegisterResponse {
+  registered: boolean;
+}
+
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+}
+
+export interface ValidateRequest {
+  token: string;
+}
+
+export interface ValidateResponse {
+  valid: boolean;
+}
+
+export interface HashPasswordRequest {
+  password: string;
+}
+
+export interface HashPasswordResponse {
+  hashedpassword: string;
+}
+
+export const AUTH_PACKAGE_NAME = "auth";
+
+export interface AuthServiceClient {
+  register(request: RegisterRequest): Observable<RegisterResponse>;
+
+  login(request: LoginRequest): Observable<LoginResponse>;
+
+  validate(request: ValidateRequest): Observable<ValidateResponse>;
+}
+
+export interface AuthServiceController {
+  register(request: RegisterRequest): Promise<RegisterResponse> | Observable<RegisterResponse> | RegisterResponse;
+
+  login(request: LoginRequest): Promise<LoginResponse> | Observable<LoginResponse> | LoginResponse;
+
+  validate(request: ValidateRequest): Promise<ValidateResponse> | Observable<ValidateResponse> | ValidateResponse;
+}
+
+export function AuthServiceControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ["register", "login", "validate"];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("AuthService", method)(constructor.prototype[method], method, descriptor);
+    }
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("AuthService", method)(constructor.prototype[method], method, descriptor);
+    }
+  };
+}
+
+export const AUTH_SERVICE_NAME = "AuthService";
+
+export interface HashServiceClient {
+  hashPassword(request: HashPasswordRequest): Observable<HashPasswordResponse>;
+}
+
+export interface HashServiceController {
+  hashPassword(
+    request: HashPasswordRequest,
+  ): Promise<HashPasswordResponse> | Observable<HashPasswordResponse> | HashPasswordResponse;
+}
+
+export function HashServiceControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ["hashPassword"];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("HashService", method)(constructor.prototype[method], method, descriptor);
+    }
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("HashService", method)(constructor.prototype[method], method, descriptor);
+    }
+  };
+}
+
+export const HASH_SERVICE_NAME = "HashService";
